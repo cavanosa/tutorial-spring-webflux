ecadfd0d70f2938d8d1db5137cbc3648ae9e2005
cavanosa <cavanosa@cavanosa.cav>
2022-12-21 14:16:46 +0100

    exception

--- src/main/java/com/tutorial/tutorialweblux/repository/ProductRepository.java
+++ src/main/java/com/tutorial/tutorialweblux/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.tutorial.tutorialweblux.repository;
 
 import com.tutorial.tutorialweblux.entity.Product;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface ProductRepository extends ReactiveCrudRepository<Product, Integer> {
+    Mono<Product> findByName(String name);
+
+    @Query("SELECT * FROM product WHERE id <> :id AND name = :name")
+    Mono<Product> repeatedName(int id, String name);
 }
--- src/main/java/com/tutorial/tutorialweblux/service/ProductService.java
+++ src/main/java/com/tutorial/tutorialweblux/service/ProductService.java
@@ -20,18 +20,28 @@ public class ProductService {
     }
 
     public Mono<Product> getById(int id) {
-        return productRepository.findById(id);
+        return productRepository.findById(id)
+                .switchIfEmpty(Mono.error(new Exception("product not found")));
     }
 
     public Mono<Product> save(Product product) {
-        return productRepository.save(product);
+        Mono<Boolean> existsName = productRepository.findByName(product.getName()).hasElement();
+        return existsName.flatMap(exists -> exists ? Mono.error(new Exception("product name already in use"))
+                : productRepository.save(product));
     }
 
     public Mono<Product> update(int id, Product product) {
-        return productRepository.save(new Product(id, product.getName(), product.getPrice()));
+        Mono<Boolean> productId = productRepository.findById(id).hasElement();
+        Mono<Boolean> productRepeatedName = productRepository.repeatedName(id, product.getName()).hasElement();
+        return productId.flatMap(
+                existsId -> existsId ?
+                        productRepeatedName.flatMap(existsName -> existsName ? Mono.error(new Exception("product name already in use"))
+                                : productRepository.save(new Product(id, product.getName(), product.getPrice())))
+        : Mono.error(new Exception("product not found")));
     }
 
     public Mono<Void> delete(int id) {
-        return productRepository.deleteById(id);
+        Mono<Boolean> productId = productRepository.findById(id).hasElement();
+        return productId.flatMap(exists -> exists ? productRepository.deleteById(id) : Mono.error(new Exception("product not found")));
     }
 }
